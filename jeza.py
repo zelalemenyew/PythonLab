fef4e8ba9588012f326824f25bc435039411d79f
jeza <jazshifa4@gmail.com>
2022-12-29 13:26:15 +0000

    edited by jeza

---
+++ jeza.py
@@ -0,0 +1,13 @@
+"""print("enter your name")
+c=input()
+def demo(names):
+    print("hello "+names)
+demo(c)"""
+print("enter first number")
+a=int(input())
+print("enter second number")
+b=int(input())
+sum= a+b
+print("sum = ",sum)
+
+
