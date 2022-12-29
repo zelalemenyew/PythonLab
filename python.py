76e2865c9c8c437dc6ca276876feca894425ce5a
danideresse <danideresse15@gmail.com>
2022-12-29 16:09:05 +0300

    mm

--- python.py
+++ python.py
@@ -1,3 +1,21 @@
 def say_hello():
     print("hello there")
 print(say_hello())
+#comment
+'''doc string'''
+height =1.79
+weight =68.7
+x,y,z ="orange","banana","cherry"
+x=y=z="orange"
+print(y)
+print(type(y))
+print(len(x))
+"""m= input()
+k= input()
+sum=int(m)+int(k)
+print (sum)"""
+name = input()
+age = input()
+number= input()
+print (f"floot")
+print(f"hello {name} your age is {age}")
